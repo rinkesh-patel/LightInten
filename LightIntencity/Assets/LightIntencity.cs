d25a4b25b51fbceb9e8712b857f3bb1fa81fa93c
Rinkesh Patel <patelrinkeshm@gmail.com>
2014-11-05 22:50:11 -0600

    Start

---
+++ LightIntencity/Assets/LightIntencity.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightIntencity : MonoBehaviour {
+
+	// Use this for initialization
+	/*void Start () {
+	
+	}
+
+	 Update is called once per frame*/
+	public GameObject target;
+	public Light source;
+	//public float distance;
+	//var other : Transform;
+
+
+
+
+	void Update () 
+	{
+		var dist = Vector3.Distance( target.transform.position, transform.position);
+		if (dist > 30) {
+			//light.intensity = 0.1f;
+			source.intensity = 0.0f;
+
+				} else {
+			//light.intensity = 5.0f;
+			source.intensity = (30/dist);
+				
+		}
+		Debug.Log ("Distance to other: " + 30/dist);
+	}
+}
